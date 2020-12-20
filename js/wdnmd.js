9f79401adbb86a2c6a5864c62835b2aa85153d97
tqy <tqy>
2020-12-20 12:13:00 +0800

    搜索

--- js/wdnmd.js
+++ js/wdnmd.js
@@ -1,26 +1,2 @@
 window.onload = function () {
-    // 获取元素
-    let soBoxtext = document.getElementById("soBoxtext");
-    let soBoxlist = document.getElementById("soBoxlist");
-    let soBoxbtn = document.getElementById("soBoxbtn");
-
-    // 监测按键
-    soBoxtext.onkeyup = function () {
-        soBoxlist.innerHTML = "";
-        let val = soBoxtext.value;
-        if (val) {
-            // 动态创建script标签
-            let oScript = document.createElement("script");
-            oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=" + val + "&cb=callback";
-            // 添加链接及回调函数
-            document.body.appendChild(oScript);
-            document.body.removeChild(oScript);
-        }
-    }
-
-    // 跳转搜索
-    soBoxbtn.onclick = function () {
-        let val = soBoxtext.value;
-        location.href = "http://www.baidu.com.cn/s?wd=" + val + "&cl=3";
-    }
 }
--- js/wdnmd2.js
+++ js/wdnmd2.js
@@ -1,42 +1,51 @@
-axios.jsonp = (url, data) => {
-    if (!url)
-        throw new Error('url is necessary')
-    const callback = 'CALLBACK' + Math.random().toString().substr(9, 18)
-    const JSONP = document.createElement('script')
-    JSONP.setAttribute('type', 'text/javascript')
-
-    const headEle = document.getElementsByTagName('head')[0]
-
-    let ret = '';
-    if (data) {
-        if (typeof data === 'string')
-            ret = '&' + data;
-        else if (typeof data === 'object') {
-            for (let key in data)
-                ret += '&' + key + '=' + encodeURIComponent(data[key]);
-        }
-        ret += '&_time=' + Date.now();
-    }
-    JSONP.src = `${url}?callback=${callback}${ret}`;
-    return new Promise((resolve, reject) => {
-        window[callback] = r => {
-            resolve(r)
-            headEle.removeChild(JSONP)
-            delete window[callback]
-        }
-        headEle.appendChild(JSONP)
-    })
-}
-
 new Vue({
     el: '#Search',
     data: {
-        test: 'wdnmd'
+        soBoxtextDom: document.querySelector("#soBoxtext"), // 输入框获取
+        soBoxlistDom: document.querySelector("#soBoxlist"), // 结果获取
+        soBoxtext: '', // 输入框内容
+        soBoxlist: [], // 搜索结果
+        soBoxlistShowNum: 8, // 最大显示结果数量
     },
     created() {
-        // this.getBing();
+        window.sessionStorage.setItem("soBoxlistShowNum", this.soBoxlistShowNum);
     },
     methods: {
-       
+        // 搜索框聚焦
+        SoFocus(b) {
+            if (b) {
+                document.querySelector(".soBoxtext").classList.add("soBoxtextFocus");
+            } else {
+                document.querySelector(".soBoxtext").classList.remove("soBoxtextFocus");
+            }
+        },
+        // 输入框内容处理
+        SoChange(e) {
+            let vm = this;
+            // 清空结果
+            document.querySelector("#soBoxlist").innerHTML = "";
+
+            // 结果显示
+            if (vm.soBoxtext) {
+                document.querySelector(".soBoxlist").classList.add("soBoxlistShow");
+            } else {
+                document.querySelector(".soBoxlist").classList.remove("soBoxlistShow");
+            }
+
+            // 回车跳转
+            if (e.keyCode == 13) {
+                window.open('https://www.baidu.com/s?wd=' + vm.soBoxtext)
+                this.keydown = '';
+            }
+
+            if (vm.soBoxtext) {
+                // 动态创建script标签
+                let oScript = document.createElement("script");
+                oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=" + vm.soBoxtext + "&cb=callback";
+                // 添加链接及回调函数
+                document.body.appendChild(oScript);
+                document.body.removeChild(oScript);
+            }
+        },
     }
 })
