878a44ddc4b954227977d197e1b5d241e47ee7d3
N0ts <mail@n0ts.cn>
2022-01-14 14:21:38 +0800

    📝[Docs]: 晚上回去再写

--- vue/src/components/setting/setting.vue
+++ vue/src/components/setting/setting.vue
@@ -1,16 +1,22 @@
 <!--
  * @Author: N0ts
  * @Date: 2022-01-14 11:06:53
- * @LastEditTime: 2022-01-14 11:06:54
+ * @LastEditTime: 2022-01-14 14:21:09
  * @Description: 设置界面
  * @FilePath: /vue/src/components/setting/setting.vue
  * @Mail：mail@n0ts.cn
 -->
 
 <template>
-    <div></div>
+    <!-- 设置界面 -->
+    <div id="setting">
+        <!-- 打开按钮 -->
+        <div class="openBtn"></div>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {} from "@element-plus/icons-vue";
+</script>
 
 <style scoped lang="stylus"></style>
